213361ccca2fda2e640615717ee8d489dfb7a005
Quwarm <59762525+Quwarm@users.noreply.github.com>
2020-01-27 17:47:06 +0300

    Update GeneticAlgorithm.h

--- GeneticAlgorithm/GeneticAlgorithm.h
+++ GeneticAlgorithm/GeneticAlgorithm.h
@@ -8,7 +8,7 @@
 template <class T>
 class GeneticAlgorithm {
  public:
-    // Инициализация основных переменных в конструкторе
+    // Initialization of the main variables in the constructor
     GeneticAlgorithm(int number_of_population_members,
                      double percent_of_best_ones_to_live,
                      std::pair<T, T> section,
@@ -23,26 +23,26 @@ class GeneticAlgorithm {
         random_gen_.seed(rd());
     }
 
-    // Инициализирует популяцию
+    // Initialize a population
     void generateInitialPopulation() {
         population_x_ = createNewPopulation();
         population_y_ = createNewPopulation();
     }
 
-    // Создает и возвращает новую популяцию
+    // Create and return a new population
     std::vector<T> createNewPopulation() {
         T minimum = section_.first;
         T maximum = section_.second;
         std::vector<T> result(number_of_population_members_);
-        std::uniform_real_distribution<> real_dist(minimum, maximum); // Равномерное непрерывное распределение
+        std::uniform_real_distribution<> real_dist(minimum, maximum);
         for (auto &k : result) {
             k = real_dist(random_gen_);
         }
         return result;
     }
 
-    // Фитнес-функция (или функция пригодности)
-    // Возвращает пары векторов лучших популяций X и Y (отбор по проценту выживаемости)
+    // Fitness function
+    // Return pairs of vectors of the best populations X and Y (selection by percentage of survival)
     std::pair<std::vector<T>, std::vector<T>> getBestMembers() {
         std::vector<T> function_values(number_of_population_members_);
         auto tempX = population_x_.begin();
@@ -50,17 +50,17 @@ class GeneticAlgorithm {
         for (auto &k : function_values) {
             k = function_(*( tempX++ ), *( tempY++ ));
         }
-        Sort(function_values, population_x_, population_y_); // Сортировка Хоара в классе
+        Sort(function_values, population_x_, population_y_); // Hoare sort in class
         auto amount_of_best_values = static_cast<int>(function_values.size() * percent_of_best_ones_to_live_);
         return {std::vector<T>(population_x_.begin(), population_x_.begin() + amount_of_best_values),
             std::vector<T>(population_y_.begin(), population_y_.begin() + amount_of_best_values)};
     }
 
-    // Мутация популяций
+    // Population mutation
     void mutate() {
         auto minimal_population_x = *std::min(population_x_.begin(), population_x_.end());
         auto minimal_population_y = *std::min(population_y_.begin(), population_y_.end());
-        std::normal_distribution<> normal_dist {0, std::min(probability_ * 1000, 0.001)}; // нормальное распределение
+        std::normal_distribution<> normal_dist {0, std::min(probability_ * 1000, 0.001)};
         for (auto &elem : population_x_) {
             elem += minimal_population_x * normal_dist(random_gen_);
         }
@@ -69,13 +69,13 @@ class GeneticAlgorithm {
         }
     }
 
-    // Рекомбинация (размножение)
+    // Recombination (reproduction)
     void crossover() {
         int population_x_length = population_x_.size();
         std::uniform_int_distribution<>
-            uniform_dist(0, population_x_length - 1); // Равномерное дискретное распределение
-        population_x_.resize(number_of_population_members_); // Увеличение
-        population_y_.resize(number_of_population_members_); // Увеличение
+            uniform_dist(0, population_x_length - 1);
+        population_x_.resize(number_of_population_members_); // Increase
+        population_y_.resize(number_of_population_members_); // Increase
         for (int i = population_x_length; i < number_of_population_members_; ++i) {
             population_x_[i] =
                 ( population_x_[uniform_dist(random_gen_)] + population_x_[uniform_dist(random_gen_)] ) / 2.0;
@@ -84,7 +84,7 @@ class GeneticAlgorithm {
         }
     }
 
-    // Поиск минимума функции (количество итераций в аргументах)
+    // Search for the minimum of the function (the number of iterations in the arguments)
     T searchMinimum(int iterations) {
         generateInitialPopulation();
         for (int i = 0; i < iterations; ++i) {
@@ -98,7 +98,7 @@ class GeneticAlgorithm {
         return function_(population_x_[minimumValueIndex], population_y_[minimumValueIndex]);
     }
 
-    // Получение индекса элемента с минимальным значением функции f(x, y)
+    // Get the index of an element with the minimum value of the function f (x, y)
     int getMinimalValueIndex() {
         std::vector<T> function_values(number_of_population_members_);
         auto tempX = population_x_.begin(), tempY = population_y_.begin();
@@ -108,7 +108,7 @@ class GeneticAlgorithm {
         return std::min(function_values.begin(), function_values.end()) - function_values.begin();
     }
 
-    // Получение X и Y координаты минимума
+    // Getting the X and Y coordinates of the minimum
     std::pair<T, T> getArgumentsOfMinimumValue() {
         auto minimum_value_index = getMinimalValueIndex();
         return {population_x_[minimum_value_index], population_y_[minimum_value_index]};
@@ -120,16 +120,16 @@ class GeneticAlgorithm {
     GeneticAlgorithm(GeneticAlgorithm &&arg) = delete;
 
  private:
-    int number_of_population_members_; // Количество популяций
-    double percent_of_best_ones_to_live_; // Процент выживаемости (лучшие выживают, остальные погибают)
-    std::pair<T, T> section_; // Ограничения областей определения
-    std::function<T(T, T)> function_; // Функция, которую необходимо минимизировать
-    double probability_; // Точность
-    std::vector<T> population_x_; // Популяция X
-    std::vector<T> population_y_; // Популяция Y
-    std::mt19937 random_gen_; // Генератор рандомных чисел
+    int number_of_population_members_; // Number of populations
+    double percent_of_best_ones_to_live_; // Survival percentage (the best survive, the rest die)
+    std::pair<T, T> section_; // Limitations of scopes
+    std::function<T(T, T)> function_; // Function to be minimized
+    double probability_; // Probability
+    std::vector<T> population_x_; // Population X
+    std::vector<T> population_y_; // Population Y
+    std::mt19937 random_gen_; // Random number generator
 
-    // Итеративная быстрая сортировка Хоара для трехмерного массива по первому вектору functionValues
+    // Iterative quick Hoar sort for three-dimensional array by first vector functionValues
     static void Sort(std::vector<T> &functionValues, std::vector<T> &populationX, std::vector<T> &populationY) {
         int Left = 0, Right = functionValues.size() - 1, L2, R2;
         T PivotValue, Temp;
